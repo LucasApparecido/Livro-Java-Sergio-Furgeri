891e6ae019d98c94beec6410ece1dabf4907ee70
Lucas Apparecido de Oliveira <lucasapparecido91@gmail.com>
2023-04-20 20:27:00 -0300

    cap1 e inicio cap2

---
+++ exercicios_poo/Cap1/Exemplo0101.java
@@ -0,0 +1,8 @@
+class Exemplo0101
+{
+    public static void main (String args[])
+    {
+        System.out.println("Inicio do aprendizado");
+        System.out.println("Hello java");
+    }
+}
---
+++ exercicios_poo/Cap2/Exemplo0201.java
@@ -0,0 +1,10 @@
+public class Exemplo0201 {
+    public static void main(String args[]) {
+        int x = 10, y = 20;
+        double dolar = 2.62;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(dolar);
+        System.out.println(x + y);
+    }
+}
