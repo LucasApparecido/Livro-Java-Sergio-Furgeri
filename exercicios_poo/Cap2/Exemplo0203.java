fc155ede0af533476fd00d60fd92e5ca70d80969
Lucas Apparecido de Oliveira <lucasapparecido91@gmail.com>
2023-04-20 20:39:30 -0300

    continuação cap 2

---
+++ exercicios_poo/Cap2/Exemplo0203.java
@@ -0,0 +1,12 @@
+public class Exemplo0203 {
+    public static void main(String args[]) {
+        int x = 10, y = 3;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("-x = " + (-x));
+        System.out.println("x/y = " + (x / y));
+        System.out.println("Resto de X por Y = " + (x % y));
+        System.out.println("Inteiro de X por Y = " + (int) (x / y));
+        System.out.println("x + 1 = " + (++x));
+    }
+}
